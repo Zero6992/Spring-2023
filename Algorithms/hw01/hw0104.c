b8f4ca0c0e9e2d3658b400ec1d3e06a01916366b
Zero6992 <mirtris0522@gmail.com>
2023-03-27 16:16:03 +0800

    feat: algo hw0104 and hw0103

--- Algorithms/hw01/hw0104.c
+++ Algorithms/hw01/hw0104.c
@@ -1,31 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 typedef struct Point {
     long long x, y;
 } Point;
 
-double area_of_polygon(Point* points, int n) {
-    double area = 0.0;
-    for (int i = 0; i < n - 1; i++) {
-        area += (points[i].x * points[i + 1].y) - (points[i + 1].x * points[i].y);
+int compare(const void *a, const void *b) {
+    Point *p1 = (Point *)a;
+    Point *p2 = (Point *)b;
+    if (p1->x != p2->x) {
+        return (p1->x > p2->x) ? 1 : -1;
+    } else {
+        return (p1->y > p2->y) ? 1 : -1;
     }
-    area += (points[n - 1].x * points[0].y) - (points[0].x * points[n - 1].y);
-    return 0.5 * abs(area);
+}
+
+bool ccw(Point a, Point b, Point c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x) > 0;
+}
+
+long double area(Point *points, int n) {
+    long double result = 0.0;
+    for (int i = 0; i < n - 1; i++) {
+        result += (long double)(points[i].x * points[i + 1].y) - (long double)(points[i + 1].x * points[i].y);
+    }
+    result += (long double)(points[n - 1].x * points[0].y) - (long double)(points[0].x * points[n - 1].y);
+    return fabsl(result) / 2.0;
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    Point* points = (Point*)malloc(n * sizeof(Point));
+    if (n < 3) {
+        printf("0\n");
+        return 0;
+    }
+
+    Point *points = malloc(n * sizeof(Point));
+    if (points == NULL) {
+        printf("Memory allocation error.\n");
+        return -1;
+    }
+
     for (int i = 0; i < n; i++) {
         scanf("%lld %lld", &points[i].x, &points[i].y);
     }
 
-    double result = area_of_polygon(points, n);
-    printf("%.1lf\n", result);
+    qsort(points, n, sizeof(Point), compare);
+
+    Point *hull = malloc(2 * n * sizeof(Point));
+    if (hull == NULL) {
+        printf("Memory allocation error.\n");
+        free(points);
+        return -1;
+    }
+
+    int k = 0;
+
+    for (int i = 0; i < n; i++) {
+        while (k >= 2 && !ccw(hull[k - 2], hull[k - 1], points[i])) {
+            k--;
+        }
+        hull[k++] = points[i];
+    }
+
+    for (int i = n - 1, t = k + 1; i > 0; i--) {
+        while (k >= t && !ccw(hull[k - 2], hull[k - 1], points[i - 1])) {
+            k--;
+        }
+        hull[k++] = points[i - 1];
+    }
+
+    long double calculated_area = area(hull, k - 1);
+    if (calculated_area - (long long)calculated_area > 0) {
+        printf("%.1Lf\n", calculated_area);
+    } else {
+        printf("%lld\n", (long long)calculated_area);
+    }
 
     free(points);
+    free(hull);
+
     return 0;
 }
--- Algorithms/hw01/test.c
+++ Algorithms/hw01/test.c
@@ -1,44 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+typedef struct Point {
+    long long x, y;
+} Point;
+
+long long compare(const void *a, const void *b) {
+    Point *p1 = (Point *)a;
+    Point *p2 = (Point *)b;
+    return (p1->x - p2->x) ? (p1->x - p2->x) : (p1->y - p2->y);
+}
+
+bool ccw(Point a, Point b, Point c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x) > 0;
+}
+
+long double area(Point *points, int n) {
+    long double result = 0.0;
+    for (int i = 0; i < n - 1; i++) {
+        result += (long double)(points[i].x * points[i + 1].y) - (long double)(points[i + 1].x * points[i].y);
+    }
+    result += (long double)(points[n - 1].x * points[0].y) - (long double)(points[0].x * points[n - 1].y);
+    return fabsl(result) / 2.0;
+}
 
 int main() {
-    int N, M, i, u, v, k;
-    scanf("%d %d", &N, &M);
+    int n;
+    scanf("%d", &n);
 
-    int* T = (int*)malloc(N * sizeof(int));
-    int* progress = (int*)calloc(N + 1, sizeof(int));
-
-    for (i = 0; i < N; ++i) {
-        scanf("%d", &T[i]);
+    Point points[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%lld %lld", &points[i].x, &points[i].y);
     }
 
-    int day;
-    for (day = 1; day <= M; ++day) {
-        scanf("%d %d %d", &u, &v, &k);
-        progress[u] += k;
-        progress[v + 1] -= k;
+    qsort(points, n, sizeof(Point), compare);
 
-        int sum = 0, goal_achieved = 1;
-        for (i = 0; i < N; ++i) {
-            sum += progress[i];
-            if (sum < T[i]) {
-                goal_achieved = 0;
-                break;
-            }
+    Point hull[2 * n];
+    int k = 0;
+
+    for (int i = 0; i < n; i++) {
+        while (k >= 2 && !ccw(hull[k - 2], hull[k - 1], points[i])) {
+            k--;
         }
+        hull[k++] = points[i];
+    }
 
-        if (goal_achieved) {
-            printf("%d\n", day);
-            break;
+    for (int i = n - 1, t = k + 1; i > 0; i--) {
+        while (k >= t && !ccw(hull[k - 2], hull[k - 1], points[i - 1])) {
+            k--;
         }
+        hull[k++] = points[i - 1];
     }
 
-    if (day > M) {
-        printf("-1\n");
+    long double calculated_area = area(hull, k - 1);
+    if (calculated_area - (long long)calculated_area > 0) {
+        printf("%.1Lf\n", calculated_area);
+    } else {
+        printf("%lld\n", (long long)calculated_area);
     }
-
-    free(T);
-    free(progress);
 
     return 0;
 }
